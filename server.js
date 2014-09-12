70dcaab223317cacf6aca4e8af5c2093a908cc54
leonardomferrari <leonardomferrari@gmail.com>
2014-09-12 15:12:54 -0300

    run2accelerate-prod

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/run2accelerate', function (req, res){
 	res.sendfile("run2accelerate/run2accelerate.html");	
 });
 
+app.get('/run2accelerate-prod', function (req, res){
+	res.sendfile("run2accelerate-prod/run2accelerate.html");	
+});
+
+
 app.get('/nxtplabs', function (req, res){
 	res.sendfile("nxtplabs/nxtplab.html");	
 });
